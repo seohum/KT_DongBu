9cb26a3e28f6a3018f89a7373d8efc0d7dd27f1a
KT_Test <qwaw11@gmail.com>
2026-08-21 15:47:44 +0900

    Connect consultation admin to Google Sheets

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.KT_CONSULT_CONFIG = {
-  FORM_ENDPOINT: "https://kt-dong-bu.vercel.app/api/consult"
+  FORM_ENDPOINT: "https://kt-dong-bu.vercel.app/api/consult",
+  SHEETS_ENDPOINT: "https://script.google.com/macros/s/AKfycbxi7OLg1zqI9BZtxOHVg5tsL_mgU_hj0zRnYY1vC92U9OGrxiwVDW9_Q6oDAIlJssYz/exec"
 };
