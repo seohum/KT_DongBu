11af8bfaba310905fcd5833ca36567973c669031
KT_Test <qwaw11@gmail.com>
2026-09-10 22:00:00 +0900

    Separate QR and main application navigation

--- config.js
+++ config.js
@@ -1,4 +1,16 @@
 window.KT_CONSULT_CONFIG = {
   FORM_ENDPOINT: "https://kt-dong-bu.vercel.app/api/consult",
-  SHEETS_ENDPOINT: "https://script.google.com/macros/s/AKfycbxi7OLg1zqI9BZtxOHVg5tsL_mgU_hj0zRnYY1vC92U9OGrxiwVDW9_Q6oDAIlJssYz/exec"
+  SHEETS_ENDPOINT: "https://script.google.com/macros/s/AKfycbxi7OLg1zqI9BZtxOHVg5tsL_mgU_hj0zRnYY1vC92U9OGrxiwVDW9_Q6oDAIlJssYz/exec",
+  FLYER_IMAGES: {
+    main: {
+      "flyer-01": "assets/flyers/flyer-01.png",
+      "flyer-02": "assets/flyers/flyer-02.png",
+      "flyer-03": "assets/flyers/flyer-03.jpg"
+    },
+    qr: {
+      "flyer-01": "assets/flyers/flyer-01.png",
+      "flyer-02": "assets/flyers/flyer-02.png",
+      "flyer-03": "assets/flyers/flyer-03.jpg"
+    }
+  }
 };
